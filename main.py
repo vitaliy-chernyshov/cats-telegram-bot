8def26c338a525f906936d087556a36a6a6c9149
Vitaly Chernyshov <druid17@yandex.ru>
2021-07-19 11:12:32 +0300

    1st version

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+load_dotenv()
+TG_TOKEN = os.getenv('TG_TOKEN')
+API_KEY = os.getenv('api_key')
+URL = 'https://api.thecatapi.com/v1/images/search'
+headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': API_KEY,
+}
+
+
+def get_cat_jpg() -> str:
+    """
+    Выдает ссылку на рандомного котика
+    """
+    params = {
+        'mime_types': 'jpg'
+    }
+    response = requests.request("GET", URL, headers=headers, params=params).json()
+    return response
+
+
+def get_cat_url(response) -> str:
+    return response[0].get('url')
+
+
+def send_cat(update, context):
+    chat = update.effective_chat
+    cat = get_cat_url(get_cat_jpg())
+    context.bot.send_photo(chat_id=chat.id, photo=cat)
+
+
+def wake_up(update, context):
+    """
+    Обработка команды /start
+    """
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='everyday is a Caturday.')
+
+def main():
+    updater = Updater(token=TG_TOKEN)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('cat', send_cat))
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
+
